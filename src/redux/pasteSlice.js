62870fc7d5ef50ccbab3ca9c0ef151f87c0797d7
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-05-20 11:58:42 +0530

    changed pastes from paste

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -5,36 +5,36 @@ import toast from 'react-hot-toast';
 export const pasteSlice = createSlice({
   name: 'paste',
   initialState: {
-    paste:localStorage.getItem("pastes")?JSON.parse(localStorage.getItem("pastes")):[], 
+     pastes: localStorage.getItem("pastes")?JSON.parse(localStorage.getItem("pastes")):[], 
   },
   reducers: {
     addToPastes: (state,action) => {
         const paste=action.payload;
-        state.paste.push(paste);
-        localStorage.setItem("pastes",JSON.stringify(state.paste))
+        state.pastes.push(paste);
+        localStorage.setItem("pastes",JSON.stringify(state.pastes))
         toast.success("Paste Created");
     },
     updateToPastes: (state,action) => {
         const paste=action.payload;
-        const index=state.paste.findIndex((item)=>item._id===paste._id);
+        const index=state.pastes.findIndex((item)=>item._id===paste._id);
 
         if(index>=0){
-            state.paste[index]=paste;
-            localStorage.setItem("pastes",JSON.stringify(state.paste));
+            state.pastes[index]=paste;
+            localStorage.setItem("pastes",JSON.stringify(state.pastes));
             toast.success("Paste Updated");
         }
     },
     resetAllPastes: (state, action) => {
-        state.paste=[];
+        state.pastes=[];
         localStorage.removeItem("pastes");
     },
     removeFromPastes:(state,action)=> {
         const pasteId=action.payload;
-        const index=state.paste.findIndex((item)=>item._id===pasteId);
-        if(index>0)
+        const index=state.pastes.findIndex((item)=>item._id===pasteId);
+        if(index>=0)
         {
-            state.paste.splice(index,1);
-            localStorage.setItem("pastes",JSON.stringify(state.paste));
+            state.pastes.splice(index,1);
+            localStorage.setItem("pastes",JSON.stringify(state.pastes));
             toast.success("Paste Deleted");
         }
     },
