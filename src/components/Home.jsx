1494405ddfcf6b1ec996f63775cdbca17c37b4a9
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-05-20 10:48:20 +0530

    added button and textarea

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router";
+import { addToPastes, updateToPastes } from "../redux/pasteSlice";
 
 const home = () => {
+  const [title, setTitle] = useState("");
+  const [value,setValue]=useState("");
+  const [searchParams,setSearchParams]=useSearchParams();
+  const pasteId=searchParams.get("pasteId");
+  const dispatch=useDispatch();
 
-    const [title,setTitle]=useState("");
+  function createPaste(){
+    const paste={
+        title:title,
+        content:value,
+        _id:pasteId||Date.now().toString(36),
+        createdAt: new Date().toISOString(),
+    }
+
+    if(pasteId){
+        dispatch(updateToPastes(paste));
+    }
+    else{
+        dispatch(addToPastes(paste));
+    }
+    setTitle("");
+    setValue('');
+    setSearchParams({});
+  }
 
   return (
-    <div>
-        <input className='rounded-xl border-black pl-2 mt-2' type="text" placeholder='Enter Title' onChange={(e)=>{setTitle(e.target.value)}} value={title}/>
+    <>
+    <div className="flex flex-row gap-4 m-auto">
+      <input
+        className="rounded-xl border pl-2 mt-2"
+        type="text"
+        placeholder="Enter Title"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        value={title}
+      />
+        <button className="button" onClick={createPaste}>{pasteId?"Update":"Create"}</button>
     </div>
-  )
-}
+    <div>
+        <textarea className="border mt-4 p-2 min-w-[500px] m-auto" value={value} onChange={(e)=>{setValue(e.target.value)}} placeholder="Enter text here" rows={20}></textarea>
+    </div>
+    </>
+  );
+};
 
-export default home
+export default home;
