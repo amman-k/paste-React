889293f431399e70e58a986bdcf8892951fe0d15
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-05-20 10:46:47 +0530

    created reducer functions

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,4 +1,6 @@
+import js from '@eslint/js';
 import { createSlice } from '@reduxjs/toolkit'
+import toast from 'react-hot-toast';
 
 export const pasteSlice = createSlice({
   name: 'paste',
@@ -7,16 +9,34 @@ export const pasteSlice = createSlice({
   },
   reducers: {
     addToPastes: (state,action) => {
-
+        const paste=action.payload;
+        state.paste.push(paste);
+        localStorage.setItem("pastes",JSON.stringify(state.paste))
+        toast.success("Paste Created");
     },
     updateToPastes: (state,action) => {
+        const paste=action.payload;
+        const index=state.paste.findIndex((item)=>item._id===paste._id);
 
+        if(index>=0){
+            state.paste[index]=paste;
+            localStorage.setItem("pastes",JSON.stringify(state.paste));
+            toast.success("Paste Updated");
+        }
     },
     resetAllPastes: (state, action) => {
-
+        state.paste=[];
+        localStorage.removeItem("pastes");
     },
     removeFromPastes:(state,action)=> {
-
+        const pasteId=action.payload;
+        const index=state.paste.findIndex((item)=>item._id===pasteId);
+        if(index>0)
+        {
+            state.paste.splice(index,1);
+            localStorage.setItem("pastes",JSON.stringify(state.paste));
+            toast.success("Paste Deleted");
+        }
     },
   }
 })
