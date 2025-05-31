3163b82b5f4fa1c107382dd8800b35c5829160cd
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-05-31 13:45:39 +0530

    created the page to view

--- src/components/ViewPaste.jsx
+++ src/components/ViewPaste.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router";
+import { addToPastes, updateToPastes } from "../redux/pasteSlice";
+import toast from "react-hot-toast";
 
 const ViewPaste = () => {
-  return (
-    <div>ViewPaste</div>
-  )
-}
 
-export default ViewPaste
+    const {id}=useSearchParams();
+    const allPastes=useSelector((state)=> state.paste.pastes);
+    const paste=allPastes.filter((p)=>p.id===id)[0]; 
+
+  return (
+    <>
+      <div className="flex flex-row gap-4 m-auto">
+        <input
+          className="rounded-xl border pl-2 mt-2"
+          type="text"
+          disabled
+          placeholder="Enter Title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={paste.title}
+        />
+      </div>
+      <div>
+        <textarea
+          className="border mt-4 p-2 min-w-[500px] m-auto"
+          value={paste.content}
+          disabled
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          placeholder="Enter text here"
+          rows={20}
+        ></textarea>
+      </div>
+    </>
+  );
+};
+
+export default ViewPaste;
