7e8b0dd99dc5e69058f160204652b98f7b232511
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-06-03 10:51:51 +0530

    added styling to text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import { NavLink } from 'react-router'
+import React from "react";
+import { NavLink } from "react-router";
 
 const navbar = () => {
   return (
-    <div className='flex flex-row gap-4'>
-        <NavLink to="/">
-            Home
+    <div className="bg-[#28443f] min-w-full">
+      <div className="flex items-center justify-center flex-row gap-4 p-5 text-white ">
+        <NavLink
+          to="/"
+          className="hover:text-[#cfef00] transition-colors duration-200"
+        >
+          Home
         </NavLink>
-        <NavLink to="/pastes"> 
-            Pastes
+        <NavLink
+          to="/pastes"
+          className="hover:text-[#cfef00] transition-colors duration-200"
+        >
+          Pastes
         </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
